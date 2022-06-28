bb238c37f01c623c4b18b91a7c9f7ead80d76dd8
Rendi Jaka Susanto <rendi.susanto@spesolution.com>
2022-06-28 09:58:15 +0700

    done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,19 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './Contact.css'
 
-const Contact = () => {
+const Contact = (props) => {
+    const { name, phone, email, photo } = props.data;
     return (
-        <></>
+        <div className='contact'>
+            <img src={photo} className='img-contact' alt='contact'></img>
+            <div className='contact-info'>
+                <p className='nameContact'>{name}</p>
+                <p className='phoneContact'>{phone}</p>
+                <p className='emailContact'>{email}</p>
+            </div>
+        </div>
     )
 }
 
